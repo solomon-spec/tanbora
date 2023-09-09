394c4453ba5f6599426dd4bec3875d403ca55ac4
solomon-abate-a2sv <solomon.abate@a2sv.org>
2023-09-09 09:29:02 +0300

    fix: add responsive for home-hero

--- frontend/src/components/home/Hero.tsx
+++ frontend/src/components/home/Hero.tsx
@@ -2,18 +2,18 @@ import Image from 'next/image'
 import placeHolder from '../../../public/place-holder.png'
 export default function Hero(){
     return (
-        <section className=" mt-10">
+        <section className="font-poppins mt-10 w-11/12 mx-auto space-y-6">
             <div className=" text-center md:w-1/2 space-y-6">
                 <h1 className="font-bold text-3xl">
                     Empowering African High School Students
                 </h1>
                 <p className="font-medium text-xl">Unlock Your Potential, Explore Your Passions, and Shape Your Future</p>
             </div>
-            <div>
+            <div className='mx-auto w-full'>
                 <Image
                     src={placeHolder}
                     alt = 'hero image'
-                    className='w-full'
+                    className='rounded-md block mx-auto w-full'
                     />
             </div>
         </section>
